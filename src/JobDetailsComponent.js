793efadfe4a6d6c44b4e33525d6c466eae78f67f
Banupriya <banupriya199828@gmail.com>
2021-08-04 21:52:15 -0700

    fix: drag and drop feature

--- src/JobDetailsComponent.js
+++ src/JobDetailsComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 import { Scrollbars } from 'react-custom-scrollbars';
 import { Placeholder } from 'semantic-ui-react';
 
@@ -14,17 +15,26 @@ class JobDetailsComponent extends React.Component {
         const { header, reloading, data } = this.props;
         return (
             <div className="orderDetails">
-                <div className="headerContent">{header} ( {data.length} )</div>
+                <div className="headerContent">{header} ( {data && data.length} )</div>
                 <Scrollbars style={{ height: 575 }} >
                     {!reloading ? data && data.map((item, index) => {
                         return (
-                            <div className="cardContainer" key={index + 1}>
-                                <div className="idStyles">{item.applicantId} <span className="applicationBtn">View Application</span></div>
-                                <div className="orderStyles">Applicant Name : {item.applicantName}</div>
-                                <div className="itemDetails">{item.experience !== 0 && <>Experience : {item.experience}</>}</div>
-                                <div className="dateStyles">Applied Date : {item.appliedDate}</div>
-                                <div className="dateStyles">Assessment Date : {item.appliedDate}</div>
-                            </div>
+                            <Draggable key={item.applicantId} draggableId={item.applicantId} index={index}>
+                                {(provided) => (
+                                    <div ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        className="cardContainer"
+                                        key={index + 1}>
+                                        <div className="idStyles">{item.applicantId} <span className="applicationBtn">View Application</span></div>
+                                        <div className="orderStyles">Applicant Name : {item.applicantName}</div>
+                                        <div className="divStyles">Position : {item.position}</div>
+                                        <div className="divStyles">{item.experience !== 0 && <>Experience : {item.experience}</>}</div>
+                                        <div className="dateStyles">Applied Date : {item.appliedDate}</div>
+                                        <div className="dateStyles">Assessment Date : {item.appliedDate}</div>
+                                    </div>
+                                )}
+                            </Draggable>
                         )
                     }) :
                         holderLength.map(() => (
